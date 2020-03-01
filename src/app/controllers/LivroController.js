add197b5a434a9406955c669412cd7e7acf7679d
Thiago Cesar <thiagohs619@gmail.com>
2020-03-01 19:39:37 -0300

    listar tudo

--- src/app/controllers/LivroController.js
+++ src/app/controllers/LivroController.js
@@ -1,6 +1,8 @@
 const Livro = require('../models/Livro');
 
 class LivroController {
+  
+  //store index put delete
 
   async store(req, res){
     const {id, titulo, autor, editora, quantidade} = await Livro.create(req.body);
@@ -8,6 +10,27 @@ class LivroController {
     res.json({
       id, titulo, autor, editora, quantidade,
     })
+  }
+
+  async index(req, res){
+    //lista usuarios
+    const livros = await Livro.findAll();
+
+    return res.json(livros)
+
+  }
+
+  async show(){
+    //exibir um unico usuario
+  }
+
+  async update(){
+    //alterar usuario
+  }
+
+  async delete(){
+    //deletar usuario
+
   }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const routes = new Router();
 const LivroController = require('./app/controllers/LivroController');
 
 routes.post('/livro', LivroController.store);
+routes.get('/livro', LivroController.index);
 
 module.exports = routes;
